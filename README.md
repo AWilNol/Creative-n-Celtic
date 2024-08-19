489bdceec17bec18b6fb9b5aead23b420ba67754
AWilNol <134312897+AWilNol@users.noreply.github.com>
2024-08-19 15:19:07 -0600

    going another direction

--- README.md
+++
@@ -1,2 +0,0 @@
-# Creative-n-Celtic
-Creative &amp; Celtic is a word art synergy that is taking up residence here. 
--- script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = onScroll;
-
-function onScroll() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.body.classList.remove("scroll-top");
-  } else {
-    document.body.classList.add("scroll-top");
-  }
-} 
