c0c3b2b093c6f25745119e96454f45944882afe1
AWilNol <134312897+AWilNol@users.noreply.github.com>
2025-12-14 04:36:33 -0700

    effects
    corrects glitches and adds some glitz

--- js/cursor-effects.js
+++ js/cursor-effects.js
@@ -1,9 +1,19 @@
 console.log("JavaScript file loaded!");
 console.log("Click detected!");
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 document.body.addEventListener('click', function(e) {
     const ripple = document.createElement('div');
     ripple.classList.add('ripple');
     document.body.appendChild(ripple);
+
+    ripple.style.setProperty('--ripple-color', getRandomColor());
 
     // Position the ripple where the click occurred
     ripple.style.left = `${e.clientX- 50}px`;
